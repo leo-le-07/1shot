b8ccb5ef65545b03ab547d14a80e7e5f2ffc2096
Leo <coder.leo.le@gmail.com>
2020-12-16 13:06:58 +0700

    Apply GA tracking G-9TF5QQDFKV

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,48 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    // Check if in production
+    const isProduction = process.env.NODE_ENV === 'production'
+
+    return {
+      ...initialProps,
+      isProduction,
+    }
+  }
   render() {
+    const { isProduction } = this.props
+
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* We only want to add the scripts if in production */}
+          {isProduction && (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
